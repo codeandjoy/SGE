c96d033a26a22d5b5754ad92908e56e2a254b049
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2022-12-22 17:55:20 +0200

    Created Spritesheet

---
+++ src/Spritesheet.cpp
@@ -0,0 +1,20 @@
+#include "Spritesheet.h"
+
+Spritesheet::Spritesheet(int textureSizeX, int textureSizeY, int numTexturesX, int numTexturesY, std::string spritesheetLocation){
+    spritesheet.loadFromFile(spritesheetLocation);
+
+    // Create coordinates vector
+    for(int i = 0; i < numTexturesY*textureSizeY; i += textureSizeY){
+        for(int j = 0; j < numTexturesX*textureSizeX; j += textureSizeX){
+            coordinates.push_back(sf::IntRect(i, j, textureSizeX, textureSizeY));
+        }
+    }
+}
+
+sf::Texture Spritesheet::getSpritesheet(){
+    return spritesheet;
+}
+
+sf::IntRect Spritesheet::getCoordinates(int n){
+    return coordinates[n];
+}
---
+++ src/Spritesheet.h
@@ -0,0 +1,20 @@
+#ifndef SPRITESHEET_H
+#define SPRITESHEET_H
+
+
+#include <SFML/Graphics.hpp>
+
+class Spritesheet{
+    public:
+        Spritesheet(int textureSizeX, int textureSizeY, int numTexturesX, int numTexturesY, std::string spritesheetLocation);
+
+        sf::Texture getSpritesheet();
+        sf::IntRect getCoordinates(int n);
+
+    private:
+        sf::Texture spritesheet;
+        std::vector<sf::IntRect> coordinates;
+};
+
+
+#endif
