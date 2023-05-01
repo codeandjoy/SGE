94e9ce6040f45fecd4bde3411afce2624d660a30
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-05-01 10:25:54 +0300

    Minor change

--- src/Entity/EntityBuilders.h
+++ src/Entity/EntityBuilders.h
@@ -7,13 +7,13 @@
 namespace sge{
     class Entity;
     
-    // Builds Entity that consists only of PhysicalObject
+    // Builds sge::Entity that consists only of sf::Sprite*
     Entity* buildPlainEntity(sf::Texture* texture, sf::IntRect textureRect, sf::Vector2f position);
 
-    // Builds Entity that consists of PhysicalObject* and "globalBounds" -> CollisionShape*
+    // Builds sge::Entity that consists of sf::Sprite* and "globalBounds" -> sge::CollisionShape*
     Entity* buildStaticEntity(sf::Texture* texture, sf::IntRect textureRect, sf::Vector2f position);
 
-    // Builds Entity that consists of PhysicalObject* and "globalBounds" -> CollisionShape*
+    // Builds sge::Entity that consists of sf::Sprite*, sge::PhysicalObject* and "globalBounds" -> sge::CollisionShape*
     //
     // PhysicalObject added continuous computations:
     // "updateVelocity" -> updateVelocityBasedOnAcceleration()
